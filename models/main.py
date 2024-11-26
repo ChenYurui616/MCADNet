61211f134e06b6134891a37597db74427e972d6c
ChenYurui616 <rui7199@163.com>
2024-11-26 14:30:22 +0800

    Update main.py

--- models/main.py
+++ models/main.py
@@ -5,14 +5,9 @@ from models.Swin import Swintransformer
 from util import *
 
 from models.decoder import Decoder
-from models.CrossAttention import CrossAttLayer
-from models.DPG import AttLayer
-from models.DFE import AugAttentionModule
-from models.MCM import MCM
-from models.ASFF import ASFF
+
 from models.SCattention import CBAM
-from models.graft_dpg import CoAttLayer
-#from models.cloformer import EfficientBlock
+from models.Coattention import CoAttLayer
 
 
 def weights_init(module):
@@ -39,21 +34,8 @@ class YYNet(nn.Module):
         self.backbone.load_state_dict(
              torch.load('/hy-tmp/pth/swin224.pth')['model'],strict = False) 
         self.mode = mode
-
-        self.net0 =  ASFF(level=0,multiplier=0.5)
-        self.net1 =  ASFF(level=1,multiplier=0.5)
-        self.net2 =  ASFF(level=2,multiplier=0.5)
-
-        self.crossattn = CrossAttLayer(512)   #[bs, 512, 14, 14]---[bs, 512, 14, 14]
-        self.mcm = MCM(512)
-        self.aug = AugAttentionModule()
-        self.fusion = AttLayer(512) #DPG:
         self.enhance = CoAttLayer(512)
         self.cbam = CBAM(512)
-
-        # self.cloformer1_2 = EfficientBlock(128, 256, 4, [1, 1, 1, 1], [7, 5, 3], 7, 7, 4, 2)
-        # self.cloformer2_3 = EfficientBlock(256, 512, 4, [1, 1, 1, 1], [7, 5, 3], 7, 7, 4, 2)
-
         
         self.decoder = Decoder()
 
@@ -86,12 +68,6 @@ class YYNet(nn.Module):
         feat, proto, weighted_x5, cormap = self.enhance(s4)  #weighted_x5 [bs, 512, 14, 14]
         feataug = self.cbam(weighted_x5)
         preds = self.decoder(feataug, s4, s3, s2, s1, H, W)       
-
-        if self.training:
-            gt = F.interpolate(gt, size=weighted_x5.size()[2:], mode='bilinear', align_corners=False)
-            feat_pos, proto_pos, weighted_x5_pos, cormap_pos = self.enhance(s4 * gt)
-            feat_neg, proto_neg, weighted_x5_neg, cormap_neg = self.enhance(s4*(1-gt))
-            return preds, proto, proto_pos, proto_neg
         
         return preds
 
